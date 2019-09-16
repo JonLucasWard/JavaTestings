a649754e7442f02c3aba8212e321ba590ff45f47
JonLucasWard <jonlucasward@gmail.com>
2019-09-16 16:09:37 -0400

    first

---
+++ TestExamples/src/test/java/com/example/Tests/AllTests.java
@@ -0,0 +1,11 @@
+package com.example.Tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ MultiplyTest.class, TestExamplesApplicationTests.class })
+public class AllTests {
+
+}
